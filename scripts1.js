67a609808e67af5e546f01701e0bd8301b28acf5
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-09-22 13:12:40 +0200

    added module type

--- scripts1.js
+++ scripts1.js
@@ -1,9 +1,14 @@
-firstName = 'John';
-age = 35;
-hobby = 'Coding';
+const firstName = "John";
+const age = 35;
+const hobby = "Coding";
 
-function myHobby () {
-  console.log(`Hello, ${firstName} (${age}). I love ${hobby}!\n`.repeat(2))
-}
+const logTwice = () => {
+    console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`);
+    console.log(`Hello, ${firstName} (${age}). I love ${hobby}!`);
+};
 
-userInfo()
+const wholeSentence = () => {
+    logTwice();
+};
+
+wholeSentence();
